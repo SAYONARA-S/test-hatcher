922b63257dda3693d663aee3ebea0c6a7710a4fb
Xthanks <thanksxxp@gmail.com>
2016-09-10 01:23:46 +0800

    上传图片

--- app/src/main/java/com/hatcher/app/ui/ApplyTeamActivity.java
+++ app/src/main/java/com/hatcher/app/ui/ApplyTeamActivity.java
@@ -1,21 +1,33 @@
 package com.hatcher.app.ui;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.hatcher.app.R;
 import com.hatcher.app.util.CommonUtil;
 import com.hatcher.app.util.Constants;
 import com.hatcher.app.util.LoginConfig;
+import com.hatcher.app.util.Options;
 import com.hatcher.app.util.ViewInject;
 import com.hatcher.app.view.RoundImageView;
+
+import java.io.File;
 
 public class ApplyTeamActivity extends BaseActivity implements OnClickListener {
 
@@ -87,7 +99,7 @@ public class ApplyTeamActivity extends BaseActivity implements OnClickListener {
                 finish();
                 break;
             case R.id.my_team_layout:
-                startActivity(new Intent(mContext, TeamInfoActivity.class));
+                showDialog();
                 break;
             case R.id.apply:
                 break;
@@ -95,6 +107,149 @@ public class ApplyTeamActivity extends BaseActivity implements OnClickListener {
             default:
                 break;
         }
+    }
+
+    private String[] items = new String[] { "选择本地图片", "拍照" };
+
+    /* 头像名称 */
+    private static final String IMAGE_FILE_NAME = "faceImage.jpg";
+
+    /* 请求码 */
+    private static final int IMAGE_REQUEST_CODE = 0;
+    private static final int CAMERA_REQUEST_CODE = 1;
+    private static final int RESULT_REQUEST_CODE = 2;
+    private final static int SCANNIN_GREQUEST_CODE = 5;
+    private String picPath = "";
+    private Cursor cursor;
+
+    private void showDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("上传图片");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case 0:
+                        Intent intentFromGallery = new Intent();
+                        intentFromGallery.setType("image/*"); // 设置文件类型
+                        intentFromGallery.setAction(Intent.ACTION_GET_CONTENT);
+                        startActivityForResult(intentFromGallery,
+                                IMAGE_REQUEST_CODE);
+                        break;
+                    case 1:
+                        Intent intentFromCapture = new Intent(
+                                MediaStore.ACTION_IMAGE_CAPTURE);
+                        // 判断存储卡是否可以用，可用进行存储
+                        if (hasSdcard()) {
+                            intentFromCapture.putExtra(MediaStore.EXTRA_OUTPUT, Uri
+                                    .fromFile(new File(Environment
+                                            .getExternalStorageDirectory(),
+                                            IMAGE_FILE_NAME)));
+                        }
+                        startActivityForResult(intentFromCapture,
+                                CAMERA_REQUEST_CODE);
+                        break;
+                }
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // 结果码不等于取消时候
+        if (resultCode != RESULT_CANCELED) {
+            switch (requestCode) {
+                case IMAGE_REQUEST_CODE:
+                    startPhotoZoom(data.getData());
+                    break;
+                case CAMERA_REQUEST_CODE:
+                    if (hasSdcard()) {
+                        File tempFile = new File(
+                                Environment.getExternalStorageDirectory(),
+                                IMAGE_FILE_NAME);
+                        startPhotoZoom(Uri.fromFile(tempFile));
+                    } else {
+                        // showToast("未找到存储卡，无法存储照片！");
+
+                    }
+                    break;
+                case RESULT_REQUEST_CODE:
+                    if (data != null) {
+                        setImageToView(data);
+                    }
+                    break;
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    /**
+     * 裁剪图片方法实现
+     *
+     * @param uri
+     */
+    public void startPhotoZoom(Uri uri) {
+        if (uri == null) {
+            Log.i("tag", "The uri is not exist.");
+        }
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        if (android.os.Build.VERSION.SDK_INT >= 19) {
+            String url= Options.getPath(activity,uri);
+            intent.setDataAndType(Uri.fromFile(new File(url)), "image/*");
+        }else{
+            intent.setDataAndType(uri, "image/*");
+        }
+        // 设置裁剪
+        intent.putExtra("crop", "true");
+        // aspectX aspectY 是宽高的比例
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+        // outputX outputY 是裁剪图片宽高
+        intent.putExtra("outputX", 320);
+        intent.putExtra("outputY", 320);
+        intent.putExtra("return-data", true);
+        startActivityForResult(intent, 2);
+
+    }
+
+    /**
+     * 保存裁剪之后的图片数据
+     *
+     */
+    private void setImageToView(Intent data) {
+        Bundle extras = data.getExtras();
+        if (extras != null) {
+            Bitmap photo = extras.getParcelable("data");
+            // Drawable drawable = new BitmapDrawable(photo);
+            my_team_icon.setImageBitmap(photo);
+            String a = MediaStore.Images.Media.insertImage(getContentResolver(), photo, null, null);
+            Uri uri = Uri.parse(a);
+
+            String[] pojo = { MediaStore.Images.Media.DATA };
+            cursor = managedQuery(uri, pojo, null, null, null);
+            if (cursor != null) {
+                int columnIndex = cursor.getColumnIndexOrThrow(pojo[0]);
+                cursor.moveToFirst();
+                picPath = cursor.getString(columnIndex);
+                // cursor.close();
+                Toast.makeText(mContext,picPath,Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private boolean hasSdcard() {
+        if (android.os.Environment.getExternalStorageState().equals(
+                android.os.Environment.MEDIA_MOUNTED)) {
+            return true;
+        } else
+            return false;
     }
 
 }
